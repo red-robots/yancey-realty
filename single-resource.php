06233be23006e948ed03c6b22b165d45aa4ccd82
Austin Crane <austincrane@gmail.com>
2018-09-21 11:02:49 -0400

    add resource single

--- single-resource.php
+++ single-resource.php
@@ -28,7 +28,74 @@ get_header(); ?>
 			</header><!-- .entry-header -->
 
 			<div class="entry-content">
-				<?php the_content(); ?>
+				<?php 
+
+				the_content(); 
+
+				$name=get_field('name');
+				$title=get_field('title');
+				$cell=get_field('cell');
+				$office=get_field('office');
+				$fax=get_field('fax');
+				$email=get_field('email');
+				$street=get_field('street');
+				$city=get_field('city');
+				$state=get_field('state');
+				$zip=get_field('zip');
+				$website=get_field('website');
+				$license=get_field('license');
+?>
+
+			<?php if($name) { ?>
+				<div class="item">
+					Name: <?php echo $name; ?>
+				</div>
+			<?php } ?>
+			<?php if($title) { ?>
+				<div class="item">
+					Title: <?php echo $title; ?>
+				</div>
+			<?php } ?>
+			<?php if($cell) { ?>
+				<div class="item">
+					Cell: <?php echo $cell; ?>
+				</div>
+			<?php } ?>
+			<?php if($office) { ?>
+				<div class="item">
+					Office: <?php echo $office; ?>
+				</div>
+			<?php } ?>
+			<?php if($fax) { ?>
+				<div class="item">
+					Fax: <?php echo $fax; ?>
+				</div>
+			<?php } ?>
+			<?php if($email) { 
+				$email=antispambot($email);
+				?>
+				<div class="item">
+					Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+				</div>
+			<?php } ?>
+			<?php if($name) { ?>
+				<div class="item">
+					Address: <br>
+					<?php if($street){echo $street;} ?><br>
+					<?php if($city){echo $city;} ?> <?php if($state){echo ', '.$state;} ?> <?php if($zip){echo $zip;} ?>
+				</div>
+			<?php } ?>
+			<?php if($website) { ?>
+				<div class="item">
+					Website: <a target="_blank" href="<?php echo $website; ?>"><?php echo $website; ?></a>
+				</div>
+			<?php } ?>
+			<?php if($license) { ?>
+				<div class="item">
+					License: <?php echo $license; ?>
+				</div>
+			<?php } ?>
+
 			</div><!-- .entry-content --> 
 
 			<footer class="entry-footer">
@@ -36,12 +103,9 @@ get_header(); ?>
 			</footer><!-- .entry-footer -->
 		</article><!-- #post-## -->
 
-			<?php the_post_navigation();
+			<?php 
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			
 
 		 endwhile; // End of the loop.
 		?>
