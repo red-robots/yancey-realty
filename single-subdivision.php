62372ca5a1e6bb55430ac3dfc3f59f412f35ea76
Austin Crane <austincrane@gmail.com>
2018-11-01 17:05:56 -0400

    add agent

--- single-subdivision.php
+++ single-subdivision.php
@@ -7,7 +7,36 @@
  * @package ACStarter
  */
 
-get_header(); ?>
+get_header(); 
+
+$post_objects = get_field('agent', false, false);
+
+if( $post_objects ): 
+
+	// override $post
+	$post = $post_objects;
+	setup_postdata( $post );
+
+		// $agentPic = get_field('picture');
+		$photo = get_field('picture');
+    	$size = 'medium';
+		$thumb = $photo['sizes'][ $size ];
+		$email = get_field('email');
+    	$spambot = antispambot($email);
+    	$phone = get_field('phone');
+    	$bio = get_field('bio');
+    	$linkedin = get_field('linkedin_link');
+    	$facebook = get_field('facebook_link');
+    	$twitter = get_field('twitter_link');
+    	$instagram = get_field('instagram_link');
+    	if( $linkedin || $facebook || $twitter || $instagram ) {
+    		$links = 'yes';
+    	} else { $links = 'no'; }
+
+endif;
+wp_reset_postdata();
+
+?>
 <div id="content-wrap">
 	<div class="wrapper">
 	<div id="primary" class="content-area">
@@ -49,46 +78,93 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <div class="widget-area">
-	<?php
-	$theID = get_the_ID();
-	$wp_query = new WP_Query();
-	$wp_query->query(array(
-	'post_type'=>'subdivision',
-	'posts_per_page' => -1,
-	'post__not_in' => array( $theID )
-	// 'paged' => $paged,
-));
-	if ($wp_query->have_posts()) : ?>
-    <h3>Other Subdivisions &amp; Complexes</h3>
-    <section class="developments grey-bg">
-    	<div class="wrapper">
-        	
-        		<?php while ($wp_query->have_posts()) : $wp_query->the_post();
-			    	
-			    	$agent = get_field('agent');
-			    	$form_chooser = get_field('form_chooser');
-			    	$floor_plans = get_field('floor_plans');
+<div class="widget">
+		<?php 
+		wp_reset_query();
 
-			    	// echo '<pre>';
-			    	// print_r($agent);
-			    	// echo '</pre>';
+		// $args = array(
+		//   'post__in' => $agentIds,
+		//   // 'orderby' => 'post_date',
+		// );
+		// $post_objects = get_posts( $args );
 
-			    ?>
+		// echo '<pre>';
+		// print_r($post_objects);
+		// echo '</pre>';
 
-				    <div class="development ">
-				    <h2><?php the_title(); ?></h2>
-				    	<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-				    		
-				    		<?php //the_excerpt(); ?>
-				    		<div class="more">
-				    			<a href="<?php the_permalink(); ?>">Go</a>
-				    		</div>
-				    </div>
-			<?php endwhile; ?>
+		shuffle( $post_objects );
+
+			foreach( $post_objects as $post ): 
+				setup_postdata($post);
+
+				$photo = get_field('picture');
+		    	$size = 'agent';
+				$thumb = $photo['sizes'][ $size ];
+				$email = get_field('email');
+		    	$spambot = antispambot($email);
+		    	$phone = get_field('phone');
+		    	$bio = get_field('bio');
+		    	$linkedin = get_field('linkedin_link');
+		    	$facebook = get_field('facebook_link');
+		    	$twitter = get_field('twitter_link');
+		    	$instagram = get_field('instagram_link');
+
+		    	if( $linkedin || $facebook || $twitter || $instagram ) {
+		    		$links = 'yes';
+		    	} else { $links = 'no'; }
+		?>
 			
-		</div>
-	</section>
-<?php endif; ?>
+			<div class="agent">
+		    	<div class="agent-photo">
+		    		<img src="<?php echo $thumb; ?>" alt="<?php echo $photo['alt']; ?>">
+		    	</div>
+		    	<h2><?php the_title(); ?></h2>
+		    	<?php if($phone) { ?>
+		    		<div class="agent-phone">
+		    			<a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+		    		</div>
+		    	<?php } ?>
+		    	<?php if($email) { ?>
+		    		<div class="agent-email">
+		    			<a href="mailto:<?php echo $spambot; ?>"><?php echo $spambot; ?></a>
+		    		</div>
+		    	<?php } ?>
+		    	<?php if( $links == 'yes') : ?>
+			    	<ul class="agent-social">
+			    		<?php if($facebook) { ?>
+			    			<li>
+				    			<a target="_blank" href="<?php echo $facebook; ?>">
+				    				<i class="fa fa-facebook" aria-hidden="true"></i>
+				    			</a>
+			    			</li>
+			    		<?php } ?>
+			    		<?php if($linkedin) { ?>
+			    			<li>
+				    			<a target="_blank" href="<?php echo $linkedin; ?>">
+				    				<i class="fa fa-linkedin" aria-hidden="true"></i>
+				    			</a>
+			    			</li>
+			    		<?php } ?>
+			    		<?php if($twitter) { ?>
+			    			<li>
+				    			<a target="_blank" href="<?php echo $twitter; ?>">
+				    				<i class="fa fa-twitter" aria-hidden="true"></i>
+				    			</a>
+			    			</li>
+			    		<?php } ?>
+			    		<?php if($instagram) { ?>
+			    			<li>
+				    			<a target="_blank" href="<?php echo $instagram; ?>">
+				    				<i class="fa fa-instagram" aria-hidden="true"></i>
+				    			</a>
+			    			</li>
+			    		<?php } ?>
+			    	</ul>
+			    <?php endif; ?>
+	    	<div class="more"><a href="<?php the_permalink(); ?>">Agent Info</a></div>
+    	</div>
+    	<?php endforeach; wp_reset_postdata(); ?>
+	
 </div>
 <?php
 //get_sidebar();?>
