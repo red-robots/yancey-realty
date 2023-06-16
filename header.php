6f527947a344b7e1858fb38727d755dd2729cb5f
accrane <austincrane@gmail.com>
2023-06-16 15:55:24 -0400

    add developments

--- header.php
+++ header.php
@@ -92,7 +92,7 @@ $phoneT = get_field('main_phone_2', 'option');
 				<?php //get_template_part('template-parts/offer-btn'); ?>
 				<!-- <a class="cash-offers" href="https://zavvie.com/yanceyrealty/offers" target="_blank">Get Cash Offers</a> -->
 				<a class="insurance" href="http://www.yanceyinsurance.com/" target="_blank">Yancey Insurance Quote</a>
-				<a class="insurance red" href="https://app.rosegatemortgage.com/app/signup/p/rosegatemortgage/brittneymorris?campaign=yanceyrealty" target="_blank">Get Pre-Approved</a>
+				<a class="insurance red" href="https://apply.lodasoft.com/register?companyGuid=42fa2c80-0ee4-4a55-890f-86e54748ddae&userGuid=eff637c2-ff79-4bfb-8e14-38ab649b6a5d&flowGuid=b02ac0c1-4cd7-4211-9ab4-2dd4e05739b2&referralSource=68638 target="_blank">Get Pre-Approved</a>
 				<!-- <form>
 		        <button target="_blank" class="color1" type="submit" formaction="https://zavvie.com/yanceyrealty/offers" style="background-color: #E31E1A; text-align:center; line-height: 36px; font-size: 14px; color:#fff; font-family: 'roboto', 'helvetica', 'arial', sans-serif; border-radius: 2px; padding: 0 16px; border:none;">Get Cash Offers</button>
 		        </form> -->
