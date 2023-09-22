610c282ff8f22a1365673bb6a3e5f22b147c3945
Lisa <lisaqdebona@gmail.com>
2023-09-22 02:24:20 -0400

    Styled navigation

--- inc/scripts.php
+++ inc/scripts.php
@@ -9,20 +9,21 @@ function acstarter_scripts() {
 		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true);
 		wp_enqueue_script('jquery');
 
-	
+	wp_register_style( 'acstarter-custom-style', get_template_directory_uri().'/css/custom.css' );
+	wp_enqueue_style( 'acstarter-custom-style' );
 
-	wp_enqueue_script( 
-			'acstarter-blocks', 
-			get_template_directory_uri() . '/assets/js/vendors.js', 
-			array(), '20120206', 
-			true 
+	wp_enqueue_script(
+			'acstarter-blocks',
+			get_template_directory_uri() . '/assets/js/vendors.js',
+			array(), '20120206',
+			true
 		);
 
-	wp_enqueue_script( 
-			'acstarter-custom', 
-			get_template_directory_uri() . '/assets/js/custom.js', 
-			array(), '20120207', 
-			true 
+	wp_enqueue_script(
+			'acstarter-custom',
+			get_template_directory_uri() . '/assets/js/custom.js',
+			array(), '20120207',
+			true
 		);
 
 
