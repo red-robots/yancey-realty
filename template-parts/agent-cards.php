ea8b4e6281c0b06e09359830c5d6c8e3806f56af
Lisa <lisaqdebona@gmail.com>
2023-09-22 23:55:51 -0400

    Updated Agents page layout

--- template-parts/agent-cards.php
+++ template-parts/agent-cards.php
@@ -17,99 +17,62 @@
 ));
 	if ($wp_query->have_posts()) : ?>
 
-	<section class="agents">
+	<section class="agents agentsList">
+    <div class="contentRow">
+      <?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
 
-    <?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
+      	$photo = get_field('picture');
+      	$size = 'agent';
+  		  //$thumb = $photo['sizes'][ $size ];
+      	$email = get_field('email');
+      	$spambot = antispambot($email);
+      	$phone = get_field('phone');
+      	$bio = get_field('bio');
+      	$linkedin = get_field('linkedin_link');
+      	$facebook = get_field('facebook_link');
+      	$twitter = get_field('twitter_link');
+      	$instagram = get_field('instagram_link');
+      	$button_text = get_field('button_text');
+      	if($button_text != '') {
+      		$btnText = $button_text;
+      	} else {
+      		$btnText = 'Agent Info';
+      	}
 
-    	$photo = get_field('picture');
-    	$size = 'agent';
-		$thumb = $photo['sizes'][ $size ];
-    	$email = get_field('email');
-    	$spambot = antispambot($email);
-    	$phone = get_field('phone');
-    	$bio = get_field('bio');
-    	$linkedin = get_field('linkedin_link');
-    	$facebook = get_field('facebook_link');
-    	$twitter = get_field('twitter_link');
-    	$instagram = get_field('instagram_link');
-    	$button_text = get_field('button_text');
-    	if($button_text != '') {
-    		$btnText = $button_text;
-    	} else {
-    		$btnText = 'Agent Info';
-    	}
+      	if( $linkedin || $facebook || $twitter || $instagram ) {
+      		$links = 'yes';
+      	} else { $links = 'no'; }
 
-    	if( $linkedin || $facebook || $twitter || $instagram ) {
-    		$links = 'yes';
-    	} else { $links = 'no'; }
-    	// $bio = get_field('bio');
-
-    	// echo '<pre>';
-    	// print_r($links);
-    	// echo '</pre>';
-
-    ?>
+      ?>
 
     <div class="agent">
-    	<div class="agent-photo">
-    		<?php if( $thumb != '' ) { ?>
-    			<img src="<?php echo $thumb; ?>" alt="<?php echo $photo['alt']; ?>">
-    		<?php } else { ?>
-    			<img src="<?php bloginfo('template_url'); ?>/images/agent-default.png" alt="Yancey Agent">
-    		<?php } ?>
-    	</div>
-    	<div class="agent-info">
-    		<h2><?php the_title(); ?></h2>
-	    	<?php if($phone) { ?>
-	    		<div class="agent-phone">
-	    			<a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
-	    		</div>
-	    	<?php } ?>
-	    	<?php if($email) { ?>
-	    		<div class="agent-email">
-	    			<a href="mailto:<?php echo $spambot; ?>"><?php echo $spambot; ?></a>
-	    		</div>
-	    	<?php } ?>
-    	</div>
-    	
-    	
-	    	<ul class="agent-social">
-	    	<?php //if( $links == 'yes') : ?>
-	    		<?php if($facebook) { ?>
-	    			<li>
-		    			<a target="_blank" href="<?php echo $facebook; ?>">
-		    				<i class="fa fa-facebook" aria-hidden="true"></i>
-		    			</a>
-	    			</li>
-	    		<?php } ?>
-	    		<?php if($linkedin) { ?>
-	    			<li>
-		    			<a target="_blank" href="<?php echo $linkedin; ?>">
-		    				<i class="fa fa-linkedin" aria-hidden="true"></i>
-		    			</a>
-	    			</li>
-	    		<?php } ?>
-	    		<?php if($twitter) { ?>
-	    			<li>
-		    			<a target="_blank" href="<?php echo $twitter; ?>">
-		    				<i class="fa fa-twitter" aria-hidden="true"></i>
-		    			</a>
-	    			</li>
-	    		<?php } ?>
-	    		<?php if($instagram) { ?>
-	    			<li>
-		    			<a target="_blank" href="<?php echo $instagram; ?>">
-		    				<i class="fa fa-instagram" aria-hidden="true"></i>
-		    			</a>
-	    			</li>
-	    		<?php } ?>
-	    		 <?php //endif; ?>
-	    	</ul>
-	   
-	    <div class="more"><a href="<?php the_permalink(); ?>"><?php echo $btnText; ?></a></div>
+      <div class="inside fadeIn wow">
+        <a href="<?php echo get_permalink() ?>" class="infolink">
+        	<figure class="agent-photo <?php echo ($photo) ? 'has-photo':'no-photo' ?>">
+            <?php if ($photo) { ?>
+            <span class="headshot">
+              <img src="<?php echo $photo['url'] ?>" alt="<?php echo $photo['title'] ?>" class="photo">
+            </span>
+            <?php } ?>
+            <img src="<?php echo get_template_directory_uri() ?>/images/resizer.png" alt="" aria-hidden="true" />
+        	</figure>
+        </a>
+      	<div class="agent-info">
+      		<h2><a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a></h2>
+  	    	<?php if($phone) { ?>
+  	    		<div class="agent-phone">
+  	    			<a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+  	    		</div>
+  	    	<?php } ?>
+  	    	<?php if($email) { ?>
+  	    		<div class="agent-email">
+  	    			<a href="mailto:<?php echo $spambot; ?>"><?php echo $spambot; ?></a>
+  	    		</div>
+  	    	<?php } ?>
+      	</div>
+      </div>
     </div>
-
-
 <?php endwhile; ?>
+  </div>
 </section>
 <?php endif; ?>
